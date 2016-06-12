e750c85b0ed4e07350c86d2a106a3cf362835427
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-06-12 12:07:36 +0300

    1.0 add Print Nobala School

--- Nobala/ViewController.swift
+++ Nobala/ViewController.swift
@@ -12,14 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Nobala School")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
