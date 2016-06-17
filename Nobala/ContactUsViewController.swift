7f38eb905a2570135f9683e1a8f730218f9eec95
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-06-17 12:44:11 +0300

    2.9 finish all button

---
+++ Nobala/ContactUsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ContactUsViewController.swift
+//  Nobala
+//
+//  Created by Abdelrahman Mohamed on 6/17/16.
+//  Copyright © 2016 Abdelrahman Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let logo = UIImage(named: "callUs.png")
+        let imageView = UIImageView(image:logo)
+        self.navigationItem.titleView = imageView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func goToHome(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+}
