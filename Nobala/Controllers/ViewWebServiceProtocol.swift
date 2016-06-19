f5e4a2c9325a93e762b905a771ecbbcfb9707458
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-06-19 13:23:46 +0300

    3.4 commit to pull

--- Nobala/Controllers/ViewWebServiceProtocol.swift
+++ Nobala/Controllers/ViewWebServiceProtocol.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
- protocol ViewWebServiceProtocol {
+protocol ViewWebServiceProtocol {
     
-    func onReceiveNews(news: [News])
+    optional func onReceiveNews(news: [News])
     func onReceiveEvents(events: [Event])
     func onReceiveMainPages(mainPages: [MainPages])
 }
