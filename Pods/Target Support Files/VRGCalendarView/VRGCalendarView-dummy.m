8c2b59c13d5b3bf5beaf7bccbca0cb369796b43f
akabel92 <ali.kabil92@gmail.com>
2016-06-28 03:26:51 +0200

    4.2 Add missing files

---
+++ Pods/Target Support Files/VRGCalendarView/VRGCalendarView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VRGCalendarView : NSObject
+@end
+@implementation PodsDummy_VRGCalendarView
+@end
---
+++ Pods/Target Support Files/VRGCalendarView/VRGCalendarView-umbrella.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+#import "NSDate+convenience.h"
+#import "NSMutableArray+convenience.h"
+#import "UIColor+expanded.h"
+#import "UIView+convenience.h"
+#import "VRGCalendarView.h"
+
+FOUNDATION_EXPORT double VRGCalendarViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char VRGCalendarViewVersionString[];
+
